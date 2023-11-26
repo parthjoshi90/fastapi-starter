d9362fd3029cd987cacf32e2479bb580365179bc
Parth Joshi <parthjoshi90@gmail.com>
2023-11-27 01:44:20 +0530

    [IMP] added the lifespan in the application

--- apps/main.py
+++ apps/main.py
@@ -1,13 +1,24 @@
 from config.settings import get_settings
+from contextlib import asynccontextmanager
+from apps.base.database import init_db
 from fastapi import FastAPI
 
+
 settings = get_settings()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield
+
 
 app = FastAPI(
     debug=settings.DEBUG,
     title=settings.APP_NAME,
     description=settings.APP_DESCRIPTION,
     version=settings.APP_VERSION,
+    lifespan=lifespan,
 )
 
 
