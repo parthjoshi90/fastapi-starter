c0f23dd67f1f1051eb6082884a0a0cbbc449ff0b
Parth Joshi <parthjoshi90@gmail.com>
2023-11-27 01:39:59 +0530

    [IMP] added the docstring for the settings

--- config/settings.py
+++ config/settings.py
@@ -6,6 +6,33 @@ from pydantic import PostgresDsn, AnyUrl, EmailStr
 
 
 class Settings(BaseSettings):
+    """
+    Settings class for configuring the FastAPI application.
+
+    Attributes:
+        DEBUG (bool): Flag indicating whether the application is in debug mode.
+        APP_VERSION (str): The version number of the FastAPI application.
+        APP_NAME (str): The name of the FastAPI application.
+        APP_DESCRIPTION (str): A brief description of the FastAPI application.
+        SECRET_KEY (str): The secret key used for cryptographic operations.
+        ACCESS_TOKEN_EXPIRE_MINUTES (int): Time duration for access token expiration in minutes.
+        DB_URI (Union[PostgresDsn, AnyUrl]): Database URI for connecting to the database.
+        SMTP_SERVER (str): SMTP server address for sending emails.
+        SMTP_PORT (int): Port number for the SMTP server.
+        SMTP_USERNAME (EmailStr): Username for authenticating with the SMTP server.
+        SMTP_PASSWORD (str): Password for authenticating with the SMTP server.
+
+    Note:
+        The values for these attributes are loaded from environment variables. If an
+        environment variable is not set, default values are used.
+
+    Example:
+        ```
+        settings = Settings()
+        print(settings.APP_NAME)
+        ```
+    """
+
     # app config
     DEBUG: bool = getenv("DEBUG", True)
     APP_VERSION: str = getenv("APP_VERSION", "0.1.0")
@@ -29,5 +56,16 @@ class Settings(BaseSettings):
 
 @lru_cache
 def get_settings() -> Settings:
+    """
+    Retrieve and return an instance of the Settings class.
+
+    This function utilizes the LRU (Least Recently Used) caching mechanism to
+    store and efficiently retrieve the settings. Subsequent calls to this function
+    with the same parameters will return the cached result, avoiding the cost of
+    recreating the Settings instance.
+
+    Returns:
+        Settings: An instance of the Settings class containing application configuration.
+    """
     settings = Settings()
     return settings
